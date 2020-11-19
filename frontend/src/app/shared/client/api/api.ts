19a1c603ddcd8ffcfc58c5e384693be58a8e4aa9
Krausz Zsolt <krausz.zsolt97@gmail.com>
2020-11-19 16:29:13 +0100

    generate api

--- frontend/src/app/shared/client/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './test.service';
-import { TestService } from './test.service';
-export * from './users.service';
-import { UsersService } from './users.service';
-export const APIS = [TestService, UsersService];
--- frontend/src/app/shared/client/model/models.ts
+++
@@ -1,3 +0,0 @@
-export * from './applicationUserDto';
-export * from './loginDto';
-export * from './registerDto';
