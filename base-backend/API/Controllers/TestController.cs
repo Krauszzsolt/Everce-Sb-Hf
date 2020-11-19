9d001ba70a309b0fe9b4bf7634d49b9fc34c7ecb
Krausz Zsolt <krausz.zsolt97@gmail.com>
2020-11-19 16:25:04 +0100

    initial backend

---
+++ base-backend/API/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using API.Attributes;
+using API.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+
+    public class TestController : BaseController
+    {
+
+        /// <summary>
+        /// Test API without authentication.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("noauth")]
+        public ActionResult<string> TestNoauth()
+        {
+            return Ok("API is working.");
+        }
+
+        /// <summary>
+        /// Test API with authentication.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("auth")]
+        [Authorize]
+        public ActionResult<string> TestAuth()
+        {
+            return Ok($"Authenticated. Hello {CurrentUser.UserName}!");
+        }
+
+        /// <summary>
+        /// Test API with authentication, admin role.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("admin-auth")]
+        [Authorize(Role: "Administrator")]
+        public ActionResult<string> TestAdminAuth()
+        {
+            return Ok($"Authenticated. Hello {CurrentUser.UserName}! You are an admin Harry!");
+        }
+
+    }
+}
---
+++ frontend/src/app/shared/client/model/models.ts
@@ -0,0 +1,3 @@
+export * from './applicationUserDto';
+export * from './loginDto';
+export * from './registerDto';
