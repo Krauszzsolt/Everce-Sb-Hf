6c1a0bebbc57fc30d032221340983b6e653f75a2
Horváth Dénes <horvathdes97@gmail.com>
2020-11-29 18:02:21 +0100

    ensure db created on startup

--- src/backend/API/Program.cs
+++ src/backend/API/Program.cs
@@ -7,9 +7,30 @@ namespace Backend
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            EnsureDbCreated(host);
+
+            host.Run();
         }
 
+        private static void EnsureDbCreated(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
